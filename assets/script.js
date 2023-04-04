96245895a3862958d9c9a214f40bdab96dfed533
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-04-03 20:48:07 -0400

    unfortunately not a lot of commits here. Adding literally everything because I did it all in one sitting

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,116 @@
-const searchInput = document.getElementById('search');
-const searchBtn = document.getElementById('searchBtn');
-const weatherContainer = document.getElementById('weatherContainer');
-const searchHistory = document.getElementById('searchHistory');
+const searchForm = document.querySelector('#search');
+const searchInput = document.querySelector('#search-input');
 
-searchBtn.addEventListener('click', searchWeather);
+searchForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	searchWeather();
+});
 
-function searchWeather() {
-	const searchTerm = searchInput.value;
-	if(searchTerm) {
-		// Store the search term in local storage
-		let searchHistoryArray = JSON.parse(localStorage.getItem('searchHistory')) || [];
-		searchHistoryArray.push(searchTerm);
-		localStorage.setItem('searchHistory', JSON.stringify(searchHistoryArray));
 
-		// Fetch weather data for the searched city
-		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=<9b7d5c66f4907f2cb41f1aeb308f77a9>&units=metric`)
-			.then(response => response.json())
-			.then(data => {
-				// Display weather data for the searched city
-				const weatherHTML = `
-					<h2>${data.name}, ${data.sys.country}</h2>
-					<p>Temperature: ${data.main.temp}°C</p>
-					<p>Humidity: ${data.main.humidity}%</p>
-					<p>Weather: ${data.weather[0].description}</p>
-				`;
-				weatherContainer.innerHTML = weatherHTML;
-			})
-			.catch(error => console.log(error));
+async function searchWeather() {
+	const city = searchInput.value;
+	
+	if (city) {
+		try {
+			const weatherData = await getWeatherData(city);
+			displayWeather(weatherData);
+			addToSearchHistory(city);
+			displaySearchHistory();
+			displayForecast(city); 
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
 
-function displaySearchHistory() {
+async function getWeatherData(city) {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=<9b7d5c66f4907f2cb41f1aeb308f77a9
+	>&units=metric`;
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+}
+
+function displayWeather(data) {
+	const weatherContainer = document.querySelector('#weather-container');
+	
+	const cityElement = document.createElement('h2');
+	cityElement.textContent = data.name;
+	weatherContainer.appendChild(cityElement);
+
+	const temperatureElement = document.createElement('p');
+	temperatureElement.innerHTML = `Temperature: ${data.main.temp}&deg;C`;
+	weatherContainer.appendChild(temperatureElement);
+
+	const descriptionElement = document.createElement('p');
+	descriptionElement.innerHTML = `Weather: ${data.weather[0].description}`;
+	weatherContainer.appendChild(descriptionElement);
+}
+
+function addToSearchHistory(city) {
 	// Get the search history array from local storage
 	let searchHistoryArray = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-	// Get the search history container element
-	const searchHistory = document.querySelector('#search-history');
+	// Add the city to the search history array
+	searchHistoryArray.unshift(city);
 
-	// Clear the search history container
-	searchHistory.innerHTML = '';
-
-	// Check if there is any search history
-	if (searchHistoryArray.length > 0) {
-
-		// Display search history
-		let searchHistoryHTML = '<h2>Search History</h2>';
-		searchHistoryArray.forEach(city => {
-			searchHistoryHTML += `<p class="search-history-item">${city}</p>`;
-		});
-		searchHistory.innerHTML = searchHistoryHTML;
-
-		// Add event listener to search history items
-		const searchHistoryItems = document.querySelectorAll('.search-history-item');
-		searchHistoryItems.forEach(item => {
-			item.addEventListener('click', () => {
-				searchInput.value = item.textContent;
-				searchWeather();
-			});
-		});
-	} else {
-		// Display a message if there is no search history
-		searchHistory.innerHTML = '<p>No search history found</p>';
-	}
+	// Store the updated search history array in local storage
+	localStorage.setItem('searchHistory', JSON.stringify(searchHistoryArray));
 }
 
-displaySearchHistory();
 
+async function displayForecast(city) {
+	// Build the URL for the 5-day forecast API call
+	const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=<9b7d5c66f4907f2cb41f1aeb308f77a9
+	>&units=metric`;
+
+	try {
+		// Fetch the forecast data for the specified city
+		const response = await fetch(url);
+		const data = await response.json();
+
+		// Get the forecast container element
+		const forecastContainer = document.querySelector('#forecast');
+
+		// Clear the forecast container
+		forecastContainer.innerHTML = '';
+
+		// Loop through the forecast data and display each
+		data.list.forEach(forecast => {
+			// Get the date and time of the forecast
+			const forecastDate = new Date(forecast.dt * 1000);
+
+			// Check if the forecast is for noon (12:00 PM) local time
+			if (forecastDate.getHours() === 12) {
+				// Create a container for the forecast data
+				const forecastElement = document.createElement('div');
+				forecastElement.classList.add('forecast-item');
+
+				// Create an element for the forecast date
+				const dateElement = document.createElement('p');
+				dateElement.textContent = forecastDate.toLocaleDateString();
+				forecastElement.appendChild(dateElement);
+
+				// Create an element for the forecast icon
+				const iconElement = document.createElement('img');
+				iconElement.src = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+				forecastElement.appendChild(iconElement);
+
+				// Create an element for the forecast temperature
+				const temperatureElement = document.createElement('p');
+				temperatureElement.innerHTML = `Temperature: ${forecast.main.temp}&deg;C`;
+				forecastElement.appendChild(temperatureElement);
+
+				// Create an element for the forecast description
+				const descriptionElement = document.createElement('p');
+				descriptionElement.innerHTML = `Weather: ${forecast.weather[0].description}`;
+				forecastElement.appendChild(descriptionElement);
+
+				// Add the forecast data to the forecast container
+				forecastContainer.appendChild(forecastElement);
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
